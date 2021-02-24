169e446e68413b51e85d1285e0b37b5dfdd9e5b4
Laurie-De-Faria <laurie.de-faria@epitech.eu>
2021-02-24 20:55:32 +0100

    [ADD]: sword : slash effect

--- Assets/Scripts/Entities/AEntity.cs
+++ Assets/Scripts/Entities/AEntity.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public abstract class AEntity : MonoBehaviour
 {
     private float life;
     private readonly float defence;
 
+    [SerializeField] private VisualEffectAsset bloodParticules;
+    private VisualEffect _bloodParticules;
+
     public AEntity(float life, float defence)
     {
         this.life = life;
         this.defence = defence;
+        _bloodParticules = new VisualEffect();
+        _bloodParticules.visualEffectAsset = bloodParticules;
     }
 
     public void DownLife(float attack)
@@ -20,10 +26,20 @@ public abstract class AEntity : MonoBehaviour
             life -= (attack - defence);
         if (life <= 0)
             DestroyEntity();
+        Bleed();
     }
 
     private void DestroyEntity()
     {
         Debug.Log("Entity die");
+        Destroy(this.gameObject);
+    }
+
+    private void Bleed()
+    {
+        if (bloodParticules)
+        {
+            //_bloodParticules.Play();
+        }
     }
 }
